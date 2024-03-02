75e37dcaf8e5be5e333da59fdf3772f484f571f2
Leonardo Lopes <leonardolivelopes2@gmail.com>
2024-03-02 11:49:09 -0300

    feat: teste cnpj with mocking

--- tests/Feature/Rules/ValidCNPJTest.php
+++ tests/Feature/Rules/ValidCNPJTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature\Rules;
 use App\Rules\ValidCNPJ;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Validator;
 use Tests\TestCase;
 
@@ -13,14 +14,28 @@ class ValidCNPJTest extends TestCase
     /** @test */
     public function it_should_check_if_the_cnpj_is_valid_and_active(): void
     {
+        Http::fake([
+            'https://brasilapi.com.br/api/cnpj/v1/06990590000123' => Http::response(
+                ['descricao_situacao_cadastral' => 'ATIVA']
+            , 200)
+        ]);
         // CNPJ GOOGLE: 06990590000123
         $validator = Validator::make(['cnpj' => '06990590000123'], ['cnpj' => new ValidCNPJ()]);
         $this->assertTrue($validator->passes());
     }
 
     /** @test */
-    public function return_false_if_cnpj_is_not_found(): void
+    public function return_false_if_cnpj_is_not_found_or_not_active(): void
     {
+        Http::fake([
+            'https://brasilapi.com.br/api/cnpj/v1/06990590000133' => Http::response(
+                [], 404
+            ),
+            'https://brasilapi.com.br/api/cnpj/v1/06990590000133' => Http::response(
+                ['descricao_situacao_cadastral' => 'INATIVA'], 200
+            ),
+        ]);
+
         $validator = Validator::make(['cnpj' => '06990590000133'], ['cnpj' => new ValidCNPJ()]);
         $this->assertTrue($validator->fails());
     }
